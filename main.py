4a74e43f879ff0c5f8505a47e8af36f30a1f01f5
xkarina2197 <xkarina2197@gmail.com>
2025-11-03 18:04:29 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+# 1-misol
+kop = 1
+for  i in range(1, 6):
+    kop *= i
+
+print(kop)
+
+# 2-misol
+kop = 1
+for  i in range(1, 11):
+    if i % 2 == 0:
+     kop *= i
+
+print(kop)
+
+# 3-misol
+n = int(input("n son kirit: "))
+kop = 1
+for  i in range(1, n+1):
+    kop *= i
+print(kop)
+
+# 4-misol
+kop = 1
+for  i in range(3, 10):
+    kop *= 1
+    
+print(kop)
+
+# 5-misol
+kop = 1
+for  i in range(1, 8):
+    if i % 2 != 0:
+        kop *= 1
+        
+print(kop)
